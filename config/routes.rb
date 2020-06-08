35fbb8ebc0f6f88f2e3a836f04c2d201634165c5
Rafael Mancini <rafael.lmancini@gmail.com>
2020-06-08 13:42:13 -0300

    Add views and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'sessions', omniauth_callbacks: 'callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :reports
+  resources :reports do
+    get :parse
+  end
   root 'profile#index'
 end
